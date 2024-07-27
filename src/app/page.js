e9fdd27550eee1132b961197b9b2dd950612ac09
arko92 <arkobhattacharjee92@gmail.com>
2024-07-27 14:26:13 +0200

    first commit tic-tac-toe

--- src/app/page.js
+++ src/app/page.js
@@ -1,113 +1,166 @@
-import Image from "next/image";
+"use client";
 
-export default function Home() {
+import { useState } from "react";
+
+/*
+Brainstorming:
+1. What components do we need?
+a) Square
+b) Tic-tac-toe board
+
+Additional task: 
+a) Track the history of all the board games
+b) Use the history to go back to any point in the history
+
+*/
+
+function Square({value, onClick}) {  // The square component takes a value X or O and an event onCLick which determines what value to display on clicking
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <button className="Square" onClick={onClick}>{value}</button>
+  );
+}
+
+function Board({squares,isXNext,onPlay}) {
+
+  // const [squares,setSquares] = useState(Array(9).fill(null));
+  // const [isXNext,setisXNext] = useState(true);
+
+  function onSquareClick(idx) {
+
+    if (squares[idx]!==null || calculateWinner(squares)) return; // We dont want to allow users to update if there is a winner
+       
+    const newSquares = [...squares];
+
+    newSquares[idx] = isXNext ? "X" : "O";
+
+    // setisXNext(!isXNext); 
+
+    // setSquares(newSquares);
+
+    onPlay(newSquares);
+
+
+  }
+
+  const winner = calculateWinner(squares);
+
+  function replay(){
+    squares = Array(9).fill(null);
+    isXNext = true;
+
+  }
+
+  return (
+    <div className="Board">
+
+      {winner?
+      ( <div className="status">
+          <div>
+            <p>Winner is {winner}</p>
+          </div>
+          <div>
+            <button className="btn" onClick={replay}>Replay</button>
+          </div>
+      </div>
+      ): (
+        <div className="status">
+          <p>Next player: {isXNext?"X":"O"}</p>
+          <button className="btn" onClick={replay}>restart</button>
         </div>
+      )}
+
+
+
+      <div className="Board-row">
+        <Square value={squares[0]} onClick = {()=>onSquareClick(0)}/>
+        <Square value={squares[1]} onClick = {()=>onSquareClick(1)}/>
+        <Square value={squares[2]} onClick = {()=>onSquareClick(2)}/>
+      </div>
+      <div className="Board-row">
+        <Square value={squares[3]} onClick = {()=>onSquareClick(3)}/>
+        <Square value={squares[4]} onClick = {()=>onSquareClick(4)}/>
+        <Square value={squares[5]} onClick = {()=>onSquareClick(5)}/>
+      </div>
+      <div className="Board-row">
+        <Square value={squares[6]} onClick = {()=>onSquareClick(6)}/>
+        <Square value={squares[7]} onClick = {()=>onSquareClick(7)}/>
+        <Square value={squares[8]} onClick = {()=>onSquareClick(8)}/>
+      </div>
+    </div>
+  );
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0,1,2], // Horizontal
+    [3,4,5], // Horizontal
+    [6,7,8], // Horizontal
+    [0,3,6], // Vertical
+    [1,4,7], // Vertical
+    [2,5,8], // Vertical
+    [2,4,6], // Diagonal
+    [0,4,8]  // Diagonal
+  ];
+
+  for (const line of lines) {
+
+    const [i,j,k] = line;
+    if(squares[i] && squares[i] === squares[j] && squares[j] === squares[k]) {
+      return squares[i]; // Winner
+    }
+
+  }
+  return null;
+}
+
+export default function Game() {
+
+  const [boardHistory, setBoardHistory] = useState([Array(9).fill(null)]); // To store the history of the board games. As currentMove increments, more arrays will be added
+
+  const [currentMove, setCurrentMove] = useState(0); // A tic-tac-toe game can have a maximum of 9 moves because it has 9 squares
+
+  const isXNext = currentMove % 2 === 0; // Check if in the current move X is to be appeared in the button. Since we decide to start with X i.e. at index 0, X will appear at even moves e.g. 0,2,4....
+
+  const currentSquares = boardHistory[currentMove]; // Get the current squares based on board history and current move
+
+  function handlePlay(newSquares) { // A function that is called everytime we make a move
+    const nextHistory = [...boardHistory.slice(0,currentMove+1),newSquares]; // Board history from the previous move + board history from the current move
+    setBoardHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  // function to jump to any move in the history
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = boardHistory.map((squares,move) => {
+    let description;
+    if (move>0) {
+      description = "Go to move # "+move;
+    } else {
+      description = "Go to game start ";
+
+    }
+    return (
+      <li key={Math.random()+move}>
+        <button onClick={()=>jumpTo(move)}>{description}</button>
+      </li>
+    );
+  })
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <h1>TIC TAC TOE</h1>
+        <br />
+        <Board squares={currentSquares} isXNext={isXNext} onPlay={handlePlay}/>
+      </div>
+      <div className="game-info">
+          <ol>{moves}</ol>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </div>
   );
 }
